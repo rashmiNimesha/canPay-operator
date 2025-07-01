1b2589aeb409b1cabc25fc2e48aae6b1b7852eb5
Nethmini Rajapaksha <nethminirajapaksha2001@gmail.com>
2025-07-02 02:29:35 +0530

    CanPay_Operator : Implement SplashActivity

--- app/src/main/java/com/example/canpay_operator/SplashActivity.java
+++ app/src/main/java/com/example/canpay_operator/SplashActivity.java
@@ -1,24 +1,24 @@
 package com.example.canpay_operator;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SplashActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Navigate to MainActivity after 2 seconds
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish(); // Close splash activity
+            }
+        }, 2000); // 2 seconds delay
     }
 }
