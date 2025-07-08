45169af71ef97bfc547864b6de844c30d9cc457a
rashmiNimesha <90458280+rashmiNimesha@users.noreply.github.com>
2025-07-08 13:36:40 +0530

    canPay-Operator: Basic setup for API integration

---
+++ app/src/main/java/com/example/canpay_operator/config/ApiConfig.java
@@ -0,0 +1,11 @@
+package com.example.canpay_operator.config;
+
+public class ApiConfig {
+    public static final String USER_ROLE = "OPERATOR";
+
+
+    public static String getBaseUrl() {
+        return "http://10.0.2.2:8081"; // Emulator
+//        return "https://api-v1-canpay.sehanw.com";
+    }
+}
