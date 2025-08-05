ca1f6dc9c024bf3fe374de35e39137ea664eb24f
rashmiNimesha <90458280+rashmiNimesha@users.noreply.github.com>
2025-08-06 01:05:23 +0530

    canPay-Operator: endpoint changed

--- app/src/main/java/com/example/canpay_operator/utils/Endpoints.java
+++ app/src/main/java/com/example/canpay_operator/utils/Endpoints.java
@@ -4,6 +4,6 @@ public class Endpoints {
     public static final String SEND_OTP = "/api/v1/auth/send-otp";
     public static final String VERIFY_OTP = "/api/v1/auth/verify-otp";
     public static final String SAVE_USER_PROFILE = "/api/v1/auth/create-profile";
-    public static final String GET_OPERATOR_ASSIGNMENT = "/api/v1/operator/assignment-status/";
+    public static final String GET_OPERATOR_ASSIGNMENT = "/api/v1/operator-assignment/assignment-status/";
     public static final String GET_OPERATOR_FINANCE_DETAILS = "/api/v1/user-service/operator/financial-details";
 }
